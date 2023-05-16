9f1887d1b43e3a43ebdbbaa680966efb81b8dfca
min9ju <k2496625@gmail.com>
2023-05-16 18:30:54 +0900

    전체가격 결과 페이지 구현

--- __init__.py
+++ __init__.py
@@ -1,9 +1,19 @@
 # __init__
 
 from flask import Flask
+from flask import render_template
+from flask import request
 
 app = Flask(__name__) # flask application 생성
 
 @app.route('/')
 def index():
-    return 'Hello world'*2
+    return 'Hello world'
+
+@app.route('/result', methods=['GET','POST'])
+def result():
+    if request.method == 'GET':
+        return render_template('main_result.html')
+    
+    if request.method == 'POST':
+        return 
