4aa7d6e247770fbc026d7e645f1ec9ae99c75a70
min9ju <k2496625@gmail.com>
2023-05-16 22:47:50 +0900

    info생성, 헤더 수정

--- __init__.py
+++ __init__.py
@@ -10,6 +10,11 @@ app = Flask(__name__) # flask application 생성
 def index():
     return render_template('main.html')
 
+# info page
+@app.route('/info')
+def info():
+    return render_template('info.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
 
