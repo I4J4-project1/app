d6334011d86e3fb0a865c180e7fb71f5a17cf2e5
이현준 <hj@ihyeonjun-ui-MacBookAir.local>
2023-05-21 16:43:17 +0900

    항공권, 렌터카 세부페이지 완료

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ app = Flask(__name__) # flask application 생성
 f_min_reco_1, f_min_reco_2 = None, None
 f_max_reco_1, f_max_reco_2 = None, None
 f_med_reco_1, f_med_reco_2 = None, None
+c_min_reco, c_med_reco, c_max_reco = None, None, None
 
 def connect_to_database():
     conn = psycopg2.connect(
@@ -38,6 +39,7 @@ def result():
     global f_min_reco_1, f_min_reco_2
     global f_max_reco_1, f_max_reco_2
     global f_med_reco_1, f_med_reco_2
+    global c_min_reco, c_med_reco, c_max_reco
 
     input_value_1 = request.args.get('goDate')
     input_value_2 = request.args.get('backDate')
@@ -181,35 +183,39 @@ def result():
         h_max_price = cur.fetchone()[0]
         
         #렌터카 최저가
+        # feature : rent_date,rent_day,return_date,return_day,car_name,oiltype,num_seat,price,reserve_avail, rent_period
         cur.execute("""
-            SELECT MIN(price)
+            SELECT *
             FROM car_total
-            WHERE num_seat <= %s AND return_date - rent_date = %s - %s
-        """, (5 if input_value_3 <= 3 else 7, input_value_2, input_value_1))
-
-        c_min_price = cur.fetchone()[0]
+            WHERE num_seat <= %s AND rent_date = %s AND return_date = %s AND reserve_avail = 1
+            ORDER BY price
+        """, (5 if input_value_3 <= 3 else 7, input_value_1, input_value_2))
+        c_min_reco = [cur.fetchone() for _ in range(5)]
+        c_min_price = c_min_reco[0][-3]
             
-            
-        
         #렌터카 중간가
         cur.execute("""
-            SELECT price
-            FROM car_total
-            WHERE num_seat <= %s AND return_date - rent_date = %s - %s
-            ORDER BY price
-        """, (5 if input_value_3 <= 3 else 7, input_value_2, input_value_1))
-
-        c_prices = cur.fetchall()
-        c_med_price = c_prices[len(c_prices)//2][0]
+            SELECT rent_date,rent_day,return_date,return_day,car_name,oiltype,num_seat,price,reserve_avail,rent_period
+            FROM (
+                SELECT *, ROW_NUMBER() OVER (ORDER BY price) AS row_num, COUNT(*) OVER () AS total_count
+                FROM car_total
+                WHERE num_seat <= %s AND rent_date = %s AND return_date = %s AND reserve_avail = 1
+            ) subquery
+            WHERE row_num BETWEEN (total_count + 1) / 2 - 2 AND (total_count + 1) / 2 + 2
+            ORDER BY price;
+        """, (5 if input_value_3 <= 3 else 7, input_value_1, input_value_2))
+        c_med_reco = [cur.fetchone() for _ in range(5)]
+        c_med_price = c_med_reco[0][-3]
         
         #렌터카 최고가
         cur.execute("""
-            SELECT MAX(price)
+            SELECT *
             FROM car_total
-            WHERE num_seat <= %s AND return_date - rent_date = %s - %s
-        """, (5 if input_value_3 <= 3 else 7, input_value_2, input_value_1))
-
-        c_max_price = cur.fetchone()[0]
+            WHERE num_seat <= %s AND rent_date = %s AND return_date = %s AND reserve_avail = 1
+            ORDER BY price DESC
+        """, (5 if input_value_3 <= 3 else 7, input_value_1, input_value_2))
+        c_max_reco = [cur.fetchone() for _ in range(5)]
+        c_max_price = c_max_reco[0][-3]
         
         #항공,호텔,렌터카의 최저가,중간가,최고가 합산
 
@@ -287,6 +293,7 @@ def dash():
 # 항공편 추천
 
 # 항공편 데이터 dict로 만들기
+# dict의 key [0 - date, 1 - day, 2 - start_time, 3 - arrival_time, 4 - airline, 5 - seat, 6 - price]
 def f_make_dict(data):
     dict_ = {}
     for i in range(1, len(data[0])):
@@ -306,13 +313,36 @@ def f_make_dict(data):
     dict_[6] = [format(price, ',') for price in dict_[6]]
     return dict_
 
+# 렌터카 데이터 dict로 만들기
+# dict의 키 [0 - rent_date, 1 - rent_day, 2 - return_date, 3 - return_day, 4 - car_name, 5 - oiltype, 6 - num_seat, 7 - price, 8 - reserve_avail, 9 - rent_period]
+def c_make_dict(data):
+    dict_ = {}
+    for i in range(4, len(data[0])-1):
+        temp = []
+        for reco in data:
+            if reco == None:
+                break
+            temp.append(reco[i])
+        
+        dict_[i] = temp
+
+    # 날짜, 가격, 시간 포멧 설정
+    dict_[0] = ('0' + str(data[0][0])[0] + '.' + str(data[0][0])[1:])
+    dict_[2] = ('0' + str(data[0][2])[0] + '.' + str(data[0][2])[1:])
+    dict_[1] = data[0][1]
+    dict_[3] = data[0][3]
+
+    dict_[7] = [format(price, ',') for price in dict_[7]]
+    dict_[9] = data[0][9]
+    return dict_
+
 @app.route('/flight_min')
 def flight_min():
     global f_min_reco_1, f_min_reco_2
     f_min_dict_1 = f_make_dict(f_min_reco_1)
     f_min_dict_2 = f_make_dict(f_min_reco_2)
-    show_num = min(len(f_min_dict_1[2]), len(f_min_dict_2[2]))
-    repeat = range(1, show_num)
+    show_num = min(len(f_min_dict_1[2]), len(f_min_dict_2[2])) - 1
+    repeat = range(1, show_num + 1)
     return render_template('min_flight.html', f_min_dict_1=f_min_dict_1, f_min_dict_2=f_min_dict_2, show_num=show_num, repeat = repeat)
 
 # 호텔 추천
@@ -323,7 +353,11 @@ def hotel_min():
 # 렌트카 추천
 @app.route('/rentcar_min')
 def rentcar_min():
-    return render_template('min_rentcar.html')
+    global c_min_reco
+    c_min_dict = c_make_dict(c_min_reco)
+    show_num = len(c_min_dict[5]) - 1
+    repeat = range(1, show_num + 1)
+    return render_template('min_rentcar.html', c_min_dict=c_min_dict, show_num=show_num, repeat=repeat)
 
 ######## 중간가 ########
 # 항공편 추천
@@ -332,8 +366,8 @@ def flight_med():
     global f_med_reco_1, f_med_reco_2
     f_med_dict_1 = f_make_dict(f_med_reco_1)
     f_med_dict_2 = f_make_dict(f_med_reco_2)
-    show_num = min(len(f_med_dict_1[2]), len(f_med_dict_2[2]))
-    repeat = range(1, show_num)
+    show_num = min(len(f_med_dict_1[2]), len(f_med_dict_2[2])) - 1
+    repeat = range(1, show_num + 1)
     return render_template('med_flight.html', f_med_dict_1=f_med_dict_1, f_med_dict_2=f_med_dict_2, show_num=show_num, repeat=repeat)
 
 # 호텔 추천
@@ -345,8 +379,11 @@ def hotel_med():
 # 렌트카 추천
 @app.route('/rentcar_med')
 def rentcar_med():
-    global c_med_price
-    return render_template('med_rentcar.html')
+    global c_med_reco
+    c_med_dict = c_make_dict(c_med_reco)
+    show_num = len(c_med_dict[5]) - 1
+    repeat = range(1, show_num + 1)
+    return render_template('med_rentcar.html', c_med_dict=c_med_dict, show_num=show_num, repeat=repeat)   
 
 ######## 최고가 ########
 # 항공편 추천
@@ -355,8 +392,8 @@ def flight_max():
     global f_max_reco_1, f_max_reco_2
     f_max_dict_1 = f_make_dict(f_max_reco_1)
     f_max_dict_2 = f_make_dict(f_max_reco_2)
-    show_num = min(len(f_max_dict_1[2]), len(f_max_dict_2[2]))
-    repeat = range(1, show_num)
+    show_num = min(len(f_max_dict_1[2]), len(f_max_dict_2[2])) - 1
+    repeat = range(1, show_num + 1)
     return render_template('max_flight.html', f_max_dict_1=f_max_dict_1, f_max_dict_2=f_max_dict_2, show_num=show_num, repeat = repeat)
 
 # 호텔 추천
@@ -368,8 +405,12 @@ def hotel_max():
 # 렌트카 추천
 @app.route('/rentcar_max')
 def rentcar_max():
-    global c_max_price
-    return render_template('max_rentcar.html')
+    global c_max_reco
+    print(c_max_reco)
+    c_max_dict = c_make_dict(c_max_reco)
+    show_num = len(c_max_dict[5]) - 1
+    repeat = range(1, show_num + 1)
+    return render_template('max_rentcar.html', c_max_dict=c_max_dict, show_num=show_num, repeat=repeat)
 
 if __name__ == '__main__':
     app.run(debug=True)
